8ec5db3a2ac124e30bdba23cbcbf6e21ebaaabdb
Grigoriy Klimanov <5g11s198088@gmail.com>
2026-02-14 12:05:23 +0300

    add app external_files.py, test_external_files.py and files transaction.csv and transactions_excel.xlsx

---
+++ main.py
@@ -0,0 +1,9 @@
+from src.external_files import read_excel, PATH_TO_FILE_XLSX
+
+# if __name__ == "__main__":
+    # result_1 = read_csv(PATH_TO_FILE_CSV)
+    # print(result_1)
+    # print(type(result_1))
+    # result_2 = read_excel(PATH_TO_FILE_XLSX)
+    # print(result_2)
+    # print(type(result_2))
